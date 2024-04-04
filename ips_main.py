7e4d8d302fbd80b76be56cc6dc86133f1d1b5547
CorbetQ <85400189+CorbetQ@users.noreply.github.com>
2024-04-04 16:29:59 +0200

    Programme Main

--- ips_main.py
+++ ips_main.py
@@ -1,1 +1,18 @@
-print("Pour le main mettre dans un fichier interdiction.log !!!!!!!!!!!!")
+#Pour le main mettre dans un fichier interdiction.log
+import logging
+import os
+
+logging.basicConfig(
+    level=logging.DEBUG, 
+    filename='interdiction.log', 
+    filemode='a', 
+    format='%(message)s'  
+)
+
+fichier = os.popen('tasklist /FO CSV')
+for ligne in fichier:
+    with open("blacklist.csv", "r") as blacklist:
+        for process in blacklist:
+            if process == ligne:
+                logging.info(ligne)
+fichier.close()
