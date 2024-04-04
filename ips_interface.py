b8e6f077af63c63a8beeac63ce9464a08af4ea40
Scorpionxpx <118664491+Scorpionxpx@users.noreply.github.com>
2024-04-04 17:46:59 +0200

    Reparation de l'interface graphique

--- ips_interface.py
+++ ips_interface.py
@@ -1,11 +1,13 @@
 import os
 import tkinter as tk # Mise en place des bibliothèques pour la gestion des fichiers
-import curl # Dans le cas ou nous devons faire des recherches webs
+import requests
+from bs4 import BeautifulSoup # Dans le cas ou nous devons faire des recherches webs
 
 
 Blacklist_PATH = "blacklist.csv" # Chemin vers la blacklist au cas ou nous devons verifier
 
-log_PATH = "interdiction.log" # Chemin vers les fichiers log
+log_FILENAME = "interdiction.log"
+log_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), log_FILENAME)
 
 liste_interdictions= []  # Variables à remplir la le nom des processus à interdire et rechercher sur internet
 
@@ -15,7 +17,7 @@ Titre = tk.Label(fenetre_root, text="Interdictions trouvés:")
 
 Button_OK = tk.Button(fenetre_root, text="OK", command=fenetre_root.destroy)
 
-Button_TaskKill = tk.Button(fenetre_root, text="Terminer les tâches", command="") # Commande à terminer pour finir les tâches interdites
+Button_TaskKill = tk.Button(fenetre_root, text="Terminer les tâches", command=fenetre_root.destroy) # Commande à terminer pour finir les tâches interdites
 
 Affichage = tk.Label(fenetre_root, text=liste_interdictions) # Affichage des interdictions des fichiers logs
 
@@ -27,9 +29,11 @@ Affichage.pack()
 # Package de tout les objets dans notre cas cela est pour l'affichage
 
 with open(log_PATH, "r") as fichier:
-    for lignes in fichier:
-        liste_interdictions.append(lignes)
-        
-print(liste_interdictions)
-        
-    
+    for ligne in fichier:
+        liste_interdictions.append(ligne.strip().replace("""\"""",""))
+
+liste_interdictions_str = "\n".join(liste_interdictions) # Conversion de la liste en une chaîne pour l'affichage
+Affichage.config(text=liste_interdictions_str)
+
+
+fenetre_root.mainloop()
