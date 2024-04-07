f3fc339dac82db04fa8db3301e69793b1dec816a
Scorpionxpx <118664491+Scorpionxpx@users.noreply.github.com>
2024-04-07 17:26:22 +0200

    mise en place des try et exept

--- ips_interface.py
+++ ips_interface.py
@@ -6,14 +6,20 @@ from bs4 import BeautifulSoup
 # Fonction pour touts les processus
 def button_kill_all():
     for processus in programmes:
-        kill_command = f"taskkill /IM {processus} /F"
-        os.system(kill_command)
+        try:
+            kill_command = f"taskkill /IM {processus} /F"
+            os.system(kill_command)
+        except Exception as e:
+            print(f"Erreur lors de l'arrêt du processus {processus}: {e}")
 
 # Fonction pour kill chaque processus
 def button_kill_spe(process_name):
     for processus in programmes:
         if process_name.lower() in processus.lower():
-            os.popen(f"taskkill /IM {processus} /F")
+            try:
+                os.popen(f"taskkill /IM {processus} /F")
+            except Exception as e:
+                print(f"Erreur lors de l'arrêt du processus {processus}: {e}")
 
 # Touts les chemins vers les fichiers
 blacklist_path = "blacklist.csv"
@@ -22,15 +28,20 @@ log_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), log_filenam
 
 # Affichage de touts les programmes dans le label de la fenetre
 programmes = []
-with open(log_path, "r") as file:
-    for line in file:
-        programmes.append(line.strip().replace('"', ""))
+try:
+    with open(log_path, "r") as file:
+        for line in file:
+            programmes.append(line.strip().replace('"', ""))
+except FileNotFoundError:
+    print("Fichier journal non trouvé")
+except Exception as e:
+    print(f"Erreur lors de la lecture du fichier journal : {e}")
 
 # Creation de la fenetre
 root = tk.Tk()
 
 # Tout les bouttons et label de la fenetre
-titre_label = tk.Label(root, text="Interdictions trouvés:")
+titre_label = tk.Label(root, text="Interdictions trouvées :")
 titre_label.pack()
 
 ok_button = tk.Button(root, text="OK", command=root.destroy)
@@ -44,7 +55,7 @@ processus_label.pack()
 
 # Boutton de kill pour chaque processus
 for processus in programmes:
-    kill_boutton = tk.Button(root, text=f"Kill {processus}", command=lambda p=processus: button_kill_spe(p))
+    kill_boutton = tk.Button(root, text=f"Tuer {processus}", command=lambda p=processus: button_kill_spe(p))
     kill_boutton.pack()
 
 # Mise en marche de la fenetre
